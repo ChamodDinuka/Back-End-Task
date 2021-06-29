5b2e043484c99c53ccbde5f43bf35d474fbf1e81
2017cs030 <chamoddinuka518@gmail.com>
2021-06-29 23:30:27 +0530

    fomat documaents and remove unwanted console

--- controllers/currency_controller.js
+++ controllers/currency_controller.js
@@ -1,62 +1,61 @@
 const axios = require('axios');
-const NodeCache = require( "node-cache" );
+const NodeCache = require("node-cache");
 const Joi = require('joi');
 const JoiCurrency = require('joi-currency-code')(require('@hapi/joi'));
 const dotenv = require('dotenv');
 
 const myCache = new NodeCache();
-let key=[];
+let key = [];
 
 const schema = Joi.object({
-    fromCurrency: Joi.string().length(3).case("upper").required(),
-    amount :Joi.number(),
-	toCurrency: Joi.string().length(3).case("upper").required()
-  });
+  fromCurrency: Joi.string().length(3).case("upper").required(),
+  amount: Joi.number(),
+  toCurrency: Joi.string().length(3).case("upper").required()
+});
 // Load env variables
-dotenv.config({path:'./back_end_config.env'});
+dotenv.config({ path: './back_end_config.env' });
 const API = process.env.API_KEY
 
-exports.currencyValue =async (req,res,next)=>{
-    value = myCache.get( "myKey" );
-    if ( value == undefined ){
+exports.currencyValue = async (req, res, next) => {
+  value = myCache.get("myKey");
+  if (value == undefined) {
     // handle miss!
-    
+
     res.status(400).send("cash empty");
-    }else{
-        try {
-            let validNumber=0;
-            const valid = await schema.validateAsync(req.body);
-            let fromCurrency = await req.body.fromCurrency;
-            let amount =await req.body.amount;
-            let toCurrency=await req.body.toCurrency;
-            for(let i=0;i<key.length;i++){
-              if(fromCurrency === key[i] || toCurrency===key[i]){
-                validNumber++;
-              }
-            }
-            if(validNumber ===2){
-            let newAmount = await (amount/value.rates[fromCurrency])*value.rates[toCurrency];
-            res.status(200).json({"amount": newAmount,"currency": toCurrency});
-            }else{
-              res.status(400).send("Invalid currency type"); 
-            }
-          }
-        catch (err) {
-            res.status(400).send("Invalid currency type");
-         }
-            
+  } else {
+    try {
+      let validNumber = 0;
+      const valid = await schema.validateAsync(req.body);
+      let fromCurrency = await req.body.fromCurrency;
+      let amount = await req.body.amount;
+      let toCurrency = await req.body.toCurrency;
+      for (let i = 0; i < key.length; i++) {
+        if (fromCurrency === key[i] || toCurrency === key[i]) {
+          validNumber++;
+        }
+      }
+      if (validNumber === 2) {
+        let newAmount = await (amount / value.rates[fromCurrency]) * value.rates[toCurrency];
+        res.status(200).json({ "amount": newAmount, "currency": toCurrency });
+      } else {
+        res.status(400).send("Invalid currency type");
+      }
     }
-    
+    catch (err) {
+      res.status(400).send("Invalid currency type");
+    }
+
+  }
+
 }
 exports.getBreeds = async () => {
-    myCache.getStats();
-    try {
-        let data = await axios.get(`http://data.fixer.io/api/latest?access_key=${API}`)
-        console.log(data.data);
-        key=Object.keys(data.data.rates);
-        await myCache.set( "myKey", data.data, 86400 );
-      return data
-    } catch (error) {
-      console.error(error)
-    }
+  myCache.getStats();
+  try {
+    let data = await axios.get(`http://data.fixer.io/api/latest?access_key=${API}`)
+    key = Object.keys(data.data.rates);
+    await myCache.set("myKey", data.data, 86400);
+    return data
+  } catch (error) {
+    console.error(error)
   }
+}
--- server.js
+++ server.js
@@ -6,35 +6,33 @@ const controllers = require('./controllers/currency_controller');
 const { response } = require('express');
 
 // Load env variables
-dotenv.config({path:'./back_end_config.env'});
+dotenv.config({ path: './back_end_config.env' });
 
 const app = express();
 
 app.use(express.json());
 
 const breeds = controllers.getBreeds()
-    .then(response => {
-      if (response) {
-        schedule.scheduleJob('*/23 * * *', function(){
-            const breeds = controllers.getBreeds()
-              .then(response => {
-                if (response) {
-                  //console.log(response.data)
-                }
-              })
-              .catch(error => {
-                console.log(error)
-              })
-          });
-        //console.log(response.data)
-      }
-    })
-    .catch(error => {
-      console.log(error.Error)
-    })
+  .then(response => {
+    if (response) {
+      schedule.scheduleJob('*/23 * * *', function () {
+        const breeds = controllers.getBreeds()
+          .then(response => {
+            if (response) {
+            }
+          })
+          .catch(error => {
+            console.log(error)
+          })
+      });
+    }
+  })
+  .catch(error => {
+    console.log(error.Error)
+  })
 
 // Mount routes
-app.use('/',currency);
+app.use('/', currency);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server running in ${PORT}`));
